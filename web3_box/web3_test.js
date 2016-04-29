c2c5bb1654b39e9815bb174e39fec487fb1d2d5b
gskerry <gregory.skerry@gmail.com>
2016-04-29 10:52:03 -0400

    consoling

--- web3_box/web3_test.js
+++ web3_box/web3_test.js
@@ -7,11 +7,19 @@ if (typeof web3 !== 'undefined') {
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
 
-
 console.log(Web3);
 
 console.log("currentProvider: ",web3.currentProvider)
 
-// var coinbase = web3.eth.coinbase;
+// web3.eth.getBlock(9, function(error, result){
+//     if(!error)
+//         console.log(result)
+//     else
+//         console.error(error);
+// })
 
-// var balance = web3.eth.getBalance(coinbase);
+var coinbase = web3.eth.coinbase;
+console.log("coinbase: ", coinbase);
+
+var balance = web3.eth.getBalance(coinbase);
+console.log("balance: ", balance);
